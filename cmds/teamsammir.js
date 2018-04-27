7bc92857a4d0f00373d616c602702c105c17fe68
MattCSmith <mrmcchow@gmail.com>
2018-04-27 17:50:45 +0100

    Added validation and response to Team Samir

--- cmds/teamsammir.js
+++ cmds/teamsammir.js
@@ -2,14 +2,20 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args, member) => {
 
+     //Wont display admin commands in DM, as it cant check user roles
+     if(message.channel.type !== "dm") {
 
-    let role = message.guild.roles.find("name", "Member of Team Samir");
+        let role = message.guild.roles.find("name", "Member of Team Samir");
 
-// or the person who made the command: 
-let signee = message.member;
+        // or the person who made the command: 
+        let signee = message.member;
 
-// Add the role!
-signee.addRole(role).catch(console.error);
+        // Add the role!
+        signee.addRole(role).catch(console.error);
+        message.channel.send(":white_check_mark:" + message.author.toString() + ", Thanks for joining Lading Ground, please see #lading-ground");
+     } else {
+        message.channel.send(':x: Sorry you can\'t execute this command in a DM, Send the command in a ZTM Channel');
+     }
 
 
 
