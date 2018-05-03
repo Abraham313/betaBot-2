46889b13556882b428083ebc7c8284ee26993560
Abdus <azad3652@gmail.com>
2018-05-03 22:54:09 +0530

    Updated web search

--- cmds/websearch.js
+++ cmds/websearch.js
@@ -1,16 +1,18 @@
 const discord = require('discord.js');
 const fetch = require('node-fetch');
 
-const bot = new discord.Client;
+const client = new discord.Client;
 let url2fetch = 'https://www.googleapis.com/customsearch/v1?key=' + process.env.CSE_API + '&cx=017576662512468239146:omuauf_lfve&q='
 
 
 
 exports.run = (client, message, args) => {
     
-    bot.on('message', message => {
-        return message.channel.send("You Typed a message");
-    })
+    let embed = new Discord.RichEmbed()
+        .setAuthor("** Markdown & Styling **")
+        .setColor("#075eea")
+        .setDescription("Discord has some built in styling options known as markdown. One that you might find the most useful is code blocks using back ticks.")
+    message.channel.send({embed: embed});
 
 };
 
