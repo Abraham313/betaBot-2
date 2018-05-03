9cb0fc413446e313d1f11bc763ff0d7969f08f24
MattCSmith <mrmcchow@gmail.com>
2018-05-03 21:49:16 +0100

    removed guild details

--- cmds/challenge.js
+++ cmds/challenge.js
@@ -3,7 +3,8 @@ exports.run = (client, message, args) => {
     if (args[0] === "challenge") return message.reply("No submission detected. Please ensure you include all the relevant information");
     args = args.join(" ");
     message.reply("Thanks for submitting your coding challenge entry!");
-    const content = `**${message.author.username}#${message.author.discriminator}** (${message.author.id}) reported:\n~~--------------------------------~~\n${args}\n~~--------------------------------~~\nOn the server: **${message.guild.name}**\nServer ID: **${message.guild.id}**`;
+    const content = `**${message.author.username}#${message.author.discriminator}** (${message.author.id}) reported:
+                    \n~~--------------------------------~~\n${args}\n~~--------------------------------~~\nOn the server: \nServer ID: `;
     client.users.get('179604866807627777').send(`${content}`);
 };
 
