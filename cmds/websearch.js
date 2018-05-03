e30a700a8a983ebb2b175931b69f588a1c06239c
Abdus <azad3652@gmail.com>
2018-05-03 22:47:23 +0530

    Updated web search

--- cmds/websearch.js
+++ cmds/websearch.js
@@ -8,30 +8,8 @@ let url2fetch = 'https://www.googleapis.com/customsearch/v1?key=' + process.env.
 
 exports.run = (client, message, args) => {
     
-    console.log(process.env.CSE_API);
     bot.on('message', message => {
-        if (message.content.split(' ').length == 1) {
-            message.channel.send(`${message.author}, Please provide a search query. I can't search **nothing** for you. \:confused:`)
-        } else {
-            let searchQuery = message.content.split(' ');
-            searchQuery.shift();
-            searchQuery = searchQuery.join('+');
-            console.log(searchQuery)
-            message.channel.send(`${message.author}, hold on! I am searching the web hard to find the best match of your query. \:nerd: `)
-            // CSE
-            fetch(url2fetch + searchQuery)
-            .then(res => res.json())
-            .then(res => {
-                for (let i=0; i<3; i++) {
-                    message.channel.send({embed: {
-                        color:  Math.floor(Math.random() * 16777214) + 1,
-                        title: res.items[i].title,
-                        url: res.items[i].link,
-                        description: res.items[i].snippet
-                    }})
-                }
-            })
-        }
+        return message.channel.send("You Typed a message");
     })
 
 };
