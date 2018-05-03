9d77a5268ead1666ba30e518013e57ba0882360f
Abdus <azad3652@gmail.com>
2018-05-03 22:09:08 +0530

    Updated web search

--- cmds/websearch.js
+++ cmds/websearch.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 const fetch = require('node-fetch');
 
-
+console.log(process.env.CSE_API);
 const bot = new discord.Client;
 let url2fetch = 'https://www.googleapis.com/customsearch/v1?key=' + process.env.CSE_API + '&cx=017576662512468239146:omuauf_lfve&q='
 
