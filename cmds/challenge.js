354ad1179de484f2460683923d355e35d80f6dca
MattCSmith <mrmcchow@gmail.com>
2018-05-04 02:19:02 +0100

    Updated challenge command

--- cmds/challenge.js
+++ cmds/challenge.js
@@ -2,18 +2,36 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
 
-    if (!args[0]) return;
-    if (args[0] === "challenge") return message.reply("No submission detected. Please ensure you include all the relevant information");
+    if (!args[0]) return message.reply("No submission detected. Please use the following structure \`\`\`=challenge Coding Challenge#5 Submission: check it out github.com/username/repo\`\`\`");
+    if (args[0] === "challenge") return message.reply("No submission detected. Please use the following structure \`\`\`=challenge Coding Challenge#5 Submission: check it out github.com/username/repo\`\`\`");
     
     submission = args.join(" ");
-    message.reply(":white_check_mark: Thanks for submitting your coding challenge entry!");
+    if (!submission.includes("github.com")) {
+        message.reply("Please ensure your include your github repo");
+    } else {
 
-    let embed = new Discord.RichEmbed()
-        .setAuthor("**${message.author.username}#${message.author.discriminator}**", message.author.avatar)
-        .setColor("#075eea")
-        .setDescription(submission)
+        //Brutally extract github repo from args
+        let gh = (submission + " ").slice(submission.indexOf("github.com/"), submission.indexOf(" ",submission.indexOf("github.com/"))).slice(11)
 
-    client.users.get('179604866807627777').send({embed: embed});
+        //Define Coding Challenge # eg. Coding-Challenge 5
+        let challengeNumber =  submission.slice(submission.indexOf("Coding Challenge#"), 19 )
+
+        //Define Embed
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.avatarURL)
+            .setColor("#075eea")
+            .setDescription(`Check out my submission for **${challengeNumber}**`)
+            .addField("GitHub Repo", `(https://github.com/${gh})`)
+            
+        //Sends Results To Someone (Currently Matt)
+        client.users.get('179604866807627777').send({embed: embed});
+
+        //Sends to a Channel (Cirrently Submission Demo)
+        client.channels.get(`441767451852800000`).send()
+
+        //Replys to User
+        message.reply(":white_check_mark: Thanks for submitting your coding challenge entry!");
+    }
 };
 
 exports.help = {
