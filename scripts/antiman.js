984c71e32baefaea51dc97c22b385709c061638d
Abdus <azad3652@gmail.com>
2018-06-07 21:45:18 +0530

    initiated cmd for ladies'

--- scripts/antiman.js
+++ scripts/antiman.js
@@ -5,5 +5,6 @@ const client = new discord.Client({disableEveryone: true});
 module.exports = () => {
     client.on('message', msg =>{
         msg.reply(`Hi, I am no more dead!`);
+        console.log(`I am working`);
     })
 }
