3aabe5789f5abcfb9c8117aec0e37f22d8a48431
MattCSmith <mrmcchow@gmail.com>
2018-06-11 02:54:47 +0100

    Re-installed node-fetch

--- betaBot-master/betaBot-master/scripts/antiman.js
+++
@@ -1,8 +0,0 @@
-const discord = require('discord.js');
-const client = new discord.Client({disableEveryone: true});
-
-const checkReaction = () => {
-    client.on('MESSAGE_REACTION_ADD', event => {
-        console.log(event);
-    })
-}
